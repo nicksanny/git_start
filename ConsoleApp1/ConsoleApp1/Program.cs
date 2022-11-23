bf4a782d27585ea6eca6585ca51eb20e79bb0191
Nick Sanny <nicksanny@gmail.com>
2022-11-23 13:39:49 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static double GetDescriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
+        static double GetFirstRoot(double a, double b, double c)
+        {
+            var discriminant = GetDescriminant(a,b,c);
+            var squareRoot=Math.Sqrt(discriminant);
+            return (-b - squareRoot) / (2 * a);
+        }
+        static void Main() 
+        {
+           var result=GetFirstRoot(1, 1, 1);
+            Console.WriteLine(result);
+        }
+    }
+
+}
